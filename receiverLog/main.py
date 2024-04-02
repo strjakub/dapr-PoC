fc602a2b4083a267f13da79ba1afbc6c165e8caa
DESKTOP-I7NIUE3\stroj <jakubstr@student.agh.edu.pl>
2024-04-02 23:22:33 +0200

    add service invocation skeleton

---
+++ receiverLog/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+from cloudevents.http import from_http
+import json
+import os
+
+app = Flask(__name__)
+app_port = os.getenv('APP_PORT', '8002')
+
+@app.route('/dapr/subscribe', methods=['GET'])
+def subscribe():
+    subscriptions = [{
+        'pubsubname': 'pubsub',
+        'topic': 'common-topic',
+        'route': 'common-topic'
+    }]
+    print('Dapr pub/sub is subscribed to: ' + json.dumps(subscriptions))
+    return jsonify(subscriptions)
+
+
+@app.route('/common-topic', methods=['POST'])
+def orders_subscriber():
+    event = from_http(request.headers, request.get_data())
+    print('Subscriber received : %s' % event.get_attributes(), flush=True)
+    return json.dumps({'success': True}), 200, {
+        'ContentType': 'application/json'}
+
+
+app.run(port=int(app_port))
---
+++ server/src/main/java/com/demo/Controller.java
@@ -0,0 +1,41 @@
+package com.demo;
+
+import io.dapr.client.DaprClient;
+import io.dapr.client.DaprClientBuilder;
+import io.dapr.client.domain.Metadata;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+import static java.util.Collections.singletonMap;
+
+@RestController
+public class Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+    private static final String MESSAGE_TTL_IN_SECONDS = "1000";
+    private static final String TOPIC_NAME = "common-topic";
+    private static final String PUBSUB_NAME = "pubsub";
+    private static final DaprClient client = new DaprClientBuilder().build();
+
+    @GetMapping("/health")
+    @ResponseStatus(code = HttpStatus.OK)
+    public String health() {
+        logger.debug("-- healthy --");
+        Random random = new Random();
+        int id = random.nextInt(999) + 1;
+        client.publishEvent(
+                PUBSUB_NAME,
+                TOPIC_NAME,
+                id,
+                singletonMap(Metadata.TTL_IN_SECONDS, MESSAGE_TTL_IN_SECONDS)).block();
+        logger.info("PubSub message sent: " + id);
+        return "healthy";
+    }
+
+}
